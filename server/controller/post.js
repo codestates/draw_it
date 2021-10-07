a325d30c5a6237d86d9860cf50f265e77dc68dde
3002 <psch0493@gmail.com>
2021-10-07 20:02:00 +0900

    post : Sequelize 설정 및 모델 생성, 마이그레이션

--- server/controller/post.js
+++ server/controller/post.js
@@ -1,5 +1,17 @@
 module.exports.postController = {
-  get: (req, res) => {
+  getAll: async (req, res) => {
+    res.status(200).send('post');
+  },
+  getById: (req, res) => {
+    res.status(200).send('post');
+  },
+  create: (req, res) => {
+    res.status(200).send('post');
+  },
+  delete: (req, res) => {
+    res.status(200).send('post');
+  },
+  check: (req, res) => {
     res.status(200).send('post');
   },
 };
