fb78bbf96c0585f8438a1c947c3257155df93666
LimJungMyeong <freewjdaud@gmail.com>
2021-10-12 00:51:45 +0900

    Loading.js 컴포넌트 구현

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,23 @@ import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Mypage from './pages/Mypage';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useEffect, useState } from "react";
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
-import Loading from './pages/Loading';
+import Loading from './components/Loading';
 
 function App() {
   // const [Login, setLogin] = useState(false);
   // const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
   return (
     <BrowserRouter>
+    {isLoading ? <Loading /> : null}
       <Switch>
         <Route exact path="/">
           <Main />
@@ -29,9 +37,7 @@ function App() {
         <Route exact path="/quiz">
           <Quiz />
         </Route>
-        <Route exact path="/loding">
-          <Loading />
-        </Route>
+        
       </Switch>
     </BrowserRouter>
   );
