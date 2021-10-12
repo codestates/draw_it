7c338db755d2738f297f7b59ef15da328187d0a0
LimJungMyeong <freewjdaud@gmail.com>
2021-10-12 15:27:55 +0900

    Home.js:이미지 수정

--- client/src/Url.js
+++ client/src/Url.js
@@ -1,1 +1,2 @@
-export const URL = process.env.REACT_APP_URL || 'http://localhost:4000';
+// export const URL = process.env.REACT_APP_URL || 'http://localhost:4000';
+export const URL = 'http://localhost:4000';
