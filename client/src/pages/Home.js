66cfe0914f86157f8d08ab2327e1d6097c831eea
3002 <psch0493@gmail.com>
2021-10-12 21:36:53 +0900

    drawit : 약간의 스타일 수정 및 기능 수정

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,16 +9,15 @@ import Useredit from './Useredit';
 import UserContext from './Context';
 
 const Home = () => {
-  
   const [quizs, setQuizs] = useState();
   const [userinfo, setUserInfo] = useState('');
   const [isOpen, setIsOpen] = useState(false);
   const history = useHistory();
   const { token, setToken } = useContext(UserContext);
   const [answer, setAnswer] = useState();
-  
+
   useEffect(() => {
-    allQuizs()
+    allQuizs();
   }, []); //userid
 
   useEffect(() => {
@@ -37,51 +36,59 @@ const Home = () => {
     history.push('/quiz');
   };
 
-  const imgDelete = (index) =>{
-    axios.delete(`${URL}/post/${index}`,{
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    }).then((res)=>{
-      const deleted = quizs.filter((quiz) => quiz.id !== index)
-      setQuizs([...deleted])
-    })
-  }
+  const imgDelete = (index) => {
+    axios
+      .delete(`${URL}/post/${index}`, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        const deleted = quizs.filter((quiz) => quiz.id !== index);
+        setQuizs([...deleted]);
+      });
+  };
   const allQuizs = () => {
-    axios.get(`${URL}/post`, {
-      headers: {
-        authorization: `Bearer ${token}`,
+    axios
+      .get(`${URL}/post`, {
+        headers: {
+          authorization: `Bearer ${token}`,
         },
       })
       .then((res) => {
         setQuizs(res.data.data);
-    }).catch((err) =>{
-      throw err
-    });
-  }
-  const myQuizs = () =>{
-    axios.get(`${URL}/post?userid=${userinfo.id}`,{
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    }).then((res)=>{
-      setQuizs(res.data.data);
-    }).catch((err)=>{
-      throw err
-    })
-  }
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+  const myQuizs = () => {
+    axios
+      .get(`${URL}/post?userid=${userinfo.id}`, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setQuizs(res.data.data);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
   const logoutHandler = () => {
     axios
       .post(`${URL}/user/signout`)
       .then((res) => {
         setToken(null);
+        localStorage.removeItem('token');
         history.push('/');
       })
       .catch((err) => {
         console.log(err);
       });
   };
-  
+
   const openHandler = () => {
     setIsOpen(!isOpen);
     scrollStop();
@@ -96,44 +103,52 @@ const Home = () => {
     }
   };
 
-  const detailQuizHandler = (index) =>{
-    axios.get(`${URL}/post/${index}`,{
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    }).then((res)=>{
-      const quizData = res.data.data
-      setAnswer(quizData.post.answer)
-      history.push('/postQuiz');
-    }).catch((err)=>{
-      console.log(err)
-    })
-  }
-  
+  const detailQuizHandler = (index) => {
+    axios
+      .get(`${URL}/post/${index}`, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        const quizData = res.data.data;
+        setAnswer(quizData.post.answer);
+        history.push('/postQuiz');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className='HomeContainer'>
+    <div className="HomeContainer">
       <header>
         <Header />
       </header>
       <section>
-        <section className='Post'>
-          <div className='Post_Header'>
+        <section className="Post">
+          <div className="Post_Header">
             <p>Community</p>
             <div className="Post-button">
               <button onClick={myQuizs}>내가 낸 문제</button>
               <button onClick={allQuizs}>전체 문제</button>
             </div>
           </div>
-          <div className='Post_Main'>
+          <div className="Post_Main">
             {quizs?.map((data) => {
               return (
                 <div key={data.id} className="QuizContainer">
                   <div className="Post-img">
-                    <img src={data.image} onClick={() => detailQuizHandler(data.id)}></img>
+                    <img
+                      src={data.image}
+                      onClick={() => detailQuizHandler(data.id)}
+                    ></img>
                   </div>
-                  <div className='QuizContainer_bottom'>
+                  <div className="QuizContainer_bottom">
                     <p>{data.User?.nickname}님의 문제</p>
-                    {data.userId === userinfo?.id ? <div onClick={()=>imgDelete(data.id)}>X</div> : null}
+                    {data.userId === userinfo?.id ? (
+                      <div onClick={() => imgDelete(data.id)}>X</div>
+                    ) : null}
                   </div>
                 </div>
               );
@@ -141,17 +156,17 @@ const Home = () => {
           </div>
         </section>
         <aside>
-          <div className='Mypage'>
+          <div className="Mypage">
             <h2>My Page</h2>
             <div>
               {userinfo?.nickname}의 정답 개수 : {userinfo?.passedPosts}
             </div>
-            <div className='Mypage_button'>
+            <div className="Mypage_button">
               <button onClick={openHandler}>회원 정보 수정</button>
               <button onClick={logoutHandler}>로그아웃</button>
             </div>
           </div>
-          <div className='Post_Draw'>
+          <div className="Post_Draw">
             <button onClick={draw}>Draw it</button>
           </div>
         </aside>
@@ -164,7 +179,6 @@ const Home = () => {
           isOpen={isOpen}
         />
       ) : null}
-
     </div>
   );
 };
